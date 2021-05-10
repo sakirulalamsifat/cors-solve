f1abfedd6909c009ee4be27d4763c4329a09ea70
Mokbul <mokbulhossain098@gmail.com>
2021-05-10 12:59:21 +0600

    add notification api with refund

--- controller/refundController.js
+++ controller/refundController.js
@@ -2,8 +2,35 @@ import express from 'express'
 import  {OK, INTERNAL_SERVER_ERROR,BAD_REQUEST,NOT_FOUND} from '../helpers/responseHelper'
 import sequelize from '../config/database'
 import {MerchentReportTemp} from '../models'
+import axios from 'axios'
+require('dotenv').config()
 
 const router = express.Router()
+
+const mlajan_notification_api_base_url = process.env.mlajan_notification_api_base_url
+
+const sendNotification = async (keyword,AMOUNT,MSISDN,REFID) => {
+
+    let query = `select * from SW_TBL_NOTIFICATION where Keyword='${keyword}'`
+    sequelize.query( query).then(async data =>{
+        if(data[0].length){
+            let msg = data[0][0].Notification_Template
+            msg = msg.replace("<AMOUNT>", AMOUNT).replace("<DESTINATIONMSISDN>", MSISDN).replace("<REFID>", REFID).replace("<SOURCEMSISDN>", MSISDN)
+            axios.post(`${mlajan_notification_api_base_url}/api/smsRouter`,{
+                "Msisdn":MSISDN,
+                "Message":msg,
+                "Keyword":keyword,
+                "SendSms":data[0][0].SendSms
+            }).then(d=>{
+                console.log(msg)
+            }).catch(e=>{
+                console.log(e)
+            })
+        }
+    })
+    
+
+}
 
 router.post('/merchentrefundtransection', (req, res)=>{
 
@@ -14,8 +41,9 @@ router.post('/merchentrefundtransection', (req, res)=>{
         let {Transaction_ID} = req.body
 
         let {common_id:MSISDN,applogin=false,ismanager=false} = req.user_info
+        console.log(req.user_info)
 
-        if(!applogin){
+        if(applogin){
 
             if(!ismanager){
                 return res.status(400).send(BAD_REQUEST(req.i18n.__('unauthorized'), null, req));
@@ -32,7 +60,7 @@ router.post('/merchentrefundtransection', (req, res)=>{
 
             if(report[0].length) {
 
-                let {Reference_ID,Amount,Source_Wallet_ID} = report[0][0] , uniqueID = Date.now()
+                let {Reference_ID,Amount,Source_Wallet_ID,Charge_Payer} = report[0][0] , uniqueID = Date.now()
 
                 Source_Wallet_ID = +Source_Wallet_ID
 
@@ -53,7 +81,7 @@ router.post('/merchentrefundtransection', (req, res)=>{
                 @Flag = 'Reversal',
                 @Transaction_ID = ${uniqueID},
                 @Reference_ID = '${Transaction_ID}',
-                @Charge_Payer = ${Source_Wallet_ID},
+                @Charge_Payer = ${Charge_Payer},
                 @New_Charge_Amount = ${Amount}
                 
               `
@@ -62,10 +90,14 @@ router.post('/merchentrefundtransection', (req, res)=>{
 
                await sequelize.query(query2).then(async v=>{
 
-                        console.log('refund store procedure resoponse : ',v)
+                        console.log('refund store procedure resoponse : ',v[0][0])
 
                         if(v[0][0].Code == 100){
                             await MerchentReportTemp.destroy({where: { Transaction_ID } })
+                            //merchent..
+                            sendNotification("FRFD",Amount,MSISDN,Transaction_ID)
+                            //customer...
+                            sendNotification("FRFD",Amount,Source_Wallet_ID,Transaction_ID)
                         }
                           
                           return res.status(200).send(OK(v[0][0], v[0][0].Msg, req))
