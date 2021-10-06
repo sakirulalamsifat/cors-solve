c6f86c6a2e5cf58386eff14b38277aa17ec756ea
Mokbul <mokbulhossain098@gmail.com>
2021-10-06 13:25:07 +0600

    auth update for bank login

--- controller/authentication.js
+++ controller/authentication.js
@@ -156,7 +156,7 @@ const somePartOfLogin = async(user,req,res,use_temp_password=false)=>{
 
     let{MSISDN,parent_id,fullname, ismanager} = user,login_datetime = new Date(), logo = null
     let is_merchent = parent_id?false:true
-    let common_id = parent_id || MSISDN
+    let common_id = parent_id || MSISDN, isBank = false, linked_agent_MSISDN = null
 
     const merchentinfo = await MerchentProfile.findOne({where:{MSISDN:common_id}})
 
@@ -165,14 +165,16 @@ const somePartOfLogin = async(user,req,res,use_temp_password=false)=>{
     if(merchentagentmap) {
 
         common_id = merchentagentmap.Agent_MSISDN
+        isBank = true
+        linked_agent_MSISDN = merchentagentmap.Agent_MSISDN
     }
-    let token = await tokenGenerate({MSISDN,parent_id,fullname,is_merchent,ismanager,common_id,login_datetime})
+    let token = await tokenGenerate({MSISDN,parent_id,isBank,fullname,is_merchent,ismanager,common_id,login_datetime})
 
     if(merchentinfo) {
          logo = merchentinfo.Logo_Image ? await getImageFullPath(merchentinfo.Logo_Image) : null
     }
 
-    return res.status(200).send(OK( {user_info:{MSISDN,fullname,is_merchent,ismanager, logo},token},null, req));
+    return res.status(200).send(OK( {user_info:{MSISDN,fullname,is_merchent,ismanager, logo,isBank,linked_agent_MSISDN},token},null, req));
 }
 
 router.post('/set_password',checkModule,MobileValidator,async(req,res)=>{
